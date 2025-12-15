4744e749e596a28b13d9631ef4a34108ebbec54d
nomnahssan-coder <mr9miofficial@gmail.com>
2025-12-15 19:17:56 +0500

    test1

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return "Hello DevOps! CI/CD is working "
+    return "Hello DevOps! CI/CD is working test 1 done check test 2 "
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
